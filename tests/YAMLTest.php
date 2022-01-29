430b0f8b28b31ce077d07873e89135cce8c8780e
Caleb Mazalevskis <maikuolan@gmail.com>
2022-01-29 19:11:06 +0800

    Improve YAML tests.

--- tests/YAMLTest.php
+++ tests/YAMLTest.php
@@ -9,7 +9,7 @@
 
 require $ClassesDir . $Case . '.php';
 
-$Expected = [
+$ExpectedForSyntax = [
     'Deep outermost' => [
         'Deep outer' => [
             'Deep inner' => [
@@ -20,12 +20,8 @@ $Expected = [
     'String foo' => 'Bar',
     'Integer foo' => 1234,
     'Float foo' => 123.4,
-    'Example numeric array' => [
-        0 => 'Bar1',
-        1 => 'Bar2',
-        2 => 'Bar3',
-        3 => 'Bar4'
-    ],
+    'Example implicit numeric array' => ['Bar0', 'Bar1', 'Bar2', 'Bar3'],
+    'Example explicit numeric array' => ['Bar0', 'Bar1', 'Bar2', 'Bar3'],
     'Example associative array' => [
         'Foo1' => 'Bar1',
         'Foo2' => 'Bar2',
@@ -33,8 +29,10 @@ $Expected = [
         'Foo4' => 'Bar4'
     ],
     'Example mixed multi-dimensional array' => [
-        0 => 'Bar1',
-        1 => 'Bar2',
+        0 => 'Bar0',
+        1 => 'Bar1',
+        2 => 'Bar2',
+        3 => 'Bar3',
         'xFooX' => 'xBarX',
         'Some int' => 4567,
         'Sub array' => [
@@ -44,7 +42,7 @@ $Expected = [
             ]
         ]
     ],
-    'Example hex-encoded data' => "Hello World (but in hex)\0",
+    'Hex-encoded string example' => "Hello World (but in hex)\0",
     'Multi-line example' => "h e l l o - w o r l d\nhello-world",
     'Folded multi-line example' => 'Hello world. This is an example.',
     'Example booleans and null' => [
@@ -55,16 +53,58 @@ $Expected = [
         'This is null' => null,
         'This is also null' => null
     ],
-    'Anchored text push' => 'Some placeholder text.',
-    'Anchored text pull' => 'Some placeholder text.',
+    'Testing anchors' => [
+        'Anchored text push' => 'Some placeholder text.',
+        'Anchored text pull' => 'Some placeholder text.'
+    ],
+    'Escaped test' => 'They said, "Our number is #123-456-789."',
     'Inserts test' => 'Hello world; Thus, from here, within this variable, a value is inserted; It should work, hopefully.',
-    'Inline array example' => ['this', 'is', 'a', 'test.', 'Foo', 'Bar', true, false, 123]
+    'Inline array example' => ['this', 'is', 'a', 'test.', 'Foo', 'Bar', true, false, 123],
+    'End of file' => ':-)'
 ];
 
-$RawYAML = file_get_contents($TestsDir . 'fixtures' . DIRECTORY_SEPARATOR . 'example.yaml');
+$ExpectedForReconstruction = [
+    'String foo' => 'Bar',
+    'Integer foo' => 1234,
+    'Float foo' => 123.4,
+    'Example implicit numeric array' => ['Bar0', 'Bar1', 'Bar2', 'Bar3'],
+    'Example associative array' => [
+        'Foo1' => 'Bar1',
+        'Foo2' => 'Bar2',
+        'Foo3' => 'Bar3',
+        'Foo4' => 'Bar4'
+    ],
+    'Example mixed multi-dimensional array' => [
+        0 => 'Bar0',
+        1 => 'Bar1',
+        'xFooX' => 'xBarX',
+        'Some int' => 4567,
+        'Sub array' => [
+            'Hello' => 'World',
+            'Sub-sub array' => [
+                'Foobar' => 'Barfoo'
+            ]
+        ]
+    ],
+    'Hex-encoded string example' => "Hello World (but in hex)\0",
+    'Multi-line example' => "h e l l o - w o r l d\nhello-world",
+    'Example booleans and null' => [
+        'This is true' => true,
+        'This is false' => false,
+        'This is null' => null
+    ],
+    'Testing anchors' => [
+        'Anchored text push' => 'Some placeholder text.',
+        'Anchored text pull' => 'Some placeholder text.'
+    ],
+    'Escaped test' => 'They said, "Our number is #123-456-789."',
+    'End of file' => ':-)'
+];
+
+$RawYAML = file_get_contents($TestsDir . 'fixtures' . DIRECTORY_SEPARATOR . 'syntax.yaml');
 
 $Object = new \Maikuolan\Common\YAML($RawYAML);
-if ($Expected !== $Object->Data) {
+if ($ExpectedForSyntax !== $Object->Data) {
     echo 'Test failed: ' . $Case . ':L' . __LINE__ . '().' . PHP_EOL;
     exit($ExitCode);
 }
@@ -79,7 +119,7 @@ if ($ProcessResult !== true) {
 }
 
 $ExitCode++;
-if ($Expected !== $Object->Data) {
+if ($ExpectedForSyntax !== $Object->Data) {
     echo 'Test failed: ' . $Case . ':L' . __LINE__ . '().' . PHP_EOL;
     exit($ExitCode);
 }
@@ -95,6 +135,22 @@ if ($Object->process($InvalidYAML, $Object->Data) !== false) {
 
 $ExitCode++;
 if ($Object->process($NoNewLineYAML, $Object->Data) !== false) {
+    echo 'Test failed: ' . $Case . ':L' . __LINE__ . '().' . PHP_EOL;
+    exit($ExitCode);
+}
+
+$RawYAML = file_get_contents($TestsDir . 'fixtures' . DIRECTORY_SEPARATOR . 'reconstruct.yaml');
+
+$Object = new \Maikuolan\Common\YAML($RawYAML);
+$ExitCode++;
+if ($ExpectedForReconstruction !== $Object->Data) {
+    echo 'Test failed: ' . $Case . ':L' . __LINE__ . '().' . PHP_EOL;
+    exit($ExitCode);
+}
+
+$Reconstructed = $Object->reconstruct($Object->Data, true, true);
+$ExitCode++;
+if ($Reconstructed !== $RawYAML) {
     echo 'Test failed: ' . $Case . ':L' . __LINE__ . '().' . PHP_EOL;
     exit($ExitCode);
 }
